6d7f55a9cbd9a90525e5cdfb2e374526176108af
Croky19 <141557212+Croky18@users.noreply.github.com>
2025-04-29 09:53:20 +0200

    Update fxmanifest.lua

--- speed-camera/fxmanifest.lua
+++ speed-camera/fxmanifest.lua
@@ -2,13 +2,11 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'croky19'
-description 'Speed Camera Script'
+description 'Speed Camera'
 version '1.0.0'
 
--- Config voor zowel client als server
 shared_script 'config.lua'
 
--- Client- en server scripts
 client_script 'client/main.lua'
 server_script 'server/main.lua'
 
